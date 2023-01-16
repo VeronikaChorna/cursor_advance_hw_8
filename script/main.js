9301c5b312137aec82ded26ec596777678a6df87
nika_chorna <veronika.chorna2000@gmail.com>
2023-01-16 23:15:26 +0200

    remove old comments

--- script/main.js
+++ script/main.js
@@ -1,8 +1,3 @@
-// У цьому завданні вам необхідно створити клас Student та розробляти методи всередині цього класу.
-
-// 1. У стдентів повинні бути наступні властивості: university, course, fullName, вони передаються при створенні студента(в конструктор).
-// 3. Створіть геттер оцінок this.marks, який повертає масив оцінок студента [5, 4, 4, 5]
-
 class Student {
      constructor(university, course, fullName) {
         this.university = university;
@@ -52,30 +47,30 @@ class Student {
 
 const student1 = new Student('Вищої Школи Психотерапії м.Одеса', '1го курсу', 'Остап Родоманський Бендер');
 
-// 2. Створіть метод this.getInfo() -> "Студент 1го курсу Вищої Школи Психотерапії м.Одеса, Остап Родоманський Бендер", метод повертає сукупну інформацію про курс, учбовий заклад та імені студента.
+// Get stidents information
 console.log('Students information:', student1.getInfo());
 
-// 4. Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку студенту. Після того, як оцінка поставлена, геттер повинен повернути масив this.marks -> [5, 4, 4, 5, 5]
+// Setter that add mark to marks
 student1.marks = 5;
 console.log(student1);
 
-// 5. Створіть метод отримання середнього балу this.getAverageMark() -> 4.6
-console.log('Avarage marks:', student1.getAverageMark());
+// Avarage mark
+console.log('Avarage mark:', student1.getAverageMark());
 
-// 6. Створіть метод this.dismiss, який "виключить" студента. Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких помилок, просто повертається завжди null замість масиву оцінок)
+// Dismiss student
 student1.dismiss();
-console.log('Students mark after dismiss:', student1.marks);
+console.log('Students marks after dismiss:', student1.marks);
 
-// 7. Створіть метод this.recover, який дозволить поновити студента
+// Recover student
 student1.recover();
-console.log('Students mark after recover:', student1.marks);
-// ----------------------------------------------------Advanced--------------------------------------------------------------------------
-// 1. Створіть новий клас BudgetStudent, який повністю наслідує клас Student
+console.log('Students marks after recover:', student1.marks);
+
+// ----------------------------------------------------Advanced--------------------------------------------------------
 
 class BudgetStudent extends Student {
     constructor(university, course, fullName) {
         super(university, course, fullName);
-        setInterval(() => this.getScolarship(), 1000);
+        setInterval(() => this.getScolarship(), 30000);
     }
 
     getScolarship() {
@@ -88,8 +83,4 @@ class BudgetStudent extends Student {
     }
 }
 
-const student2 = new BudgetStudent('Вищої Школи Психотерапії м.Одеса', '1го курсу', 'Остап Родоманський Бендер');
-// 2. Бюджетний студент може отримати стипендію за допомогою методу this.getScholarship. Отримання стипендії супроводжується виведенням інформації в консоль: Ви отримали 1400 грн. стипендії
-// 3. Метод отримання стипендії автоматично викликається кожні 30 секунд післе створення об'єкту. Підказка: викликайте його в constructor
-// 4. Студент отримує стипендію тільки в тому випадку, якщо середній бал у нього вище або дорівнює 4.0
-// 5. Якщо студента виключено, він не отримує стипендію (думаю це було і так очевидно:) )
+const student2 = new BudgetStudent('Академії Мистецтв м.Львів', '2го курсу', 'Анна Вітвіцька Тест');
