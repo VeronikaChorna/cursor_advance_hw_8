74497f43db5596e5cd11c19659ea674ef4785711
nika_chorna <veronika.chorna2000@gmail.com>
2023-01-16 16:33:40 +0200

    try to add dismiss and recover

--- script/main.js
+++ script/main.js
@@ -14,38 +14,44 @@ class Student {
         return (`Студент ${this.course} ${this.university}, ${this.fullName}`);
      }
 
-     studentMarks = [5, 4, 4, 5];
+     #marks = [5, 4, 4, 5];
 
      get marks() {
-        return this.studentMarks;
+        return this.#marks;
      }
 
      set marks(mark) {
-        this.studentMarks.push(mark);
+        return this.#marks.push(mark);
      }
 
      getAverageMark() {
-      const marks = this.marks.flat();
+      const marks = this.#marks;
+      const iterator = marks.values();
       let sum = 0;
-        for (const value of marks) {
-            sum += value;
+        for (const mark of iterator) {
+            sum += mark;
         }
       const avgMakrs = (sum / marks.length).toFixed(1);
       return avgMakrs;
      }
 
      dismiss() {
-        this.marks = null;
+      this.#marks = null;
      }
 
      recover() {
-
+      this.#marks = this.#marks;
      }
+     
+
+
 }
 
 const student1 = new Student('Вищої Школи Психотерапії м.Одеса', '1го курсу', 'Остап Родоманський Бендер');
+
 // 2. Створіть метод this.getInfo() -> "Студент 1го курсу Вищої Школи Психотерапії м.Одеса, Остап Родоманський Бендер", метод повертає сукупну інформацію про курс, учбовий заклад та імені студента.
 console.log('Students information:', student1.getInfo());
+
 // 4. Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку студенту. Після того, як оцінка поставлена, геттер повинен повернути масив this.marks -> [5, 4, 4, 5, 5]
 student1.marks = 5;
 console.log(student1);
@@ -54,10 +60,15 @@ console.log(student1);
 console.log('Avarage mark:', student1.getAverageMark());
 
 // 6. Створіть метод this.dismiss, який "виключить" студента. Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких помилок, просто повертається завжди null замість масиву оцінок)
-console.log(student1.dismiss());
+student1.dismiss();
+
+
+console.log(student1.marks);
 
 // 7. Створіть метод this.recover, який дозволить поновити студента
+student1.recover();
 
+console.log(student1.marks);
 // ----------------------------------------------------Advanced--------------------------------------------------------------------------
 // 1. Створіть новий клас BudgetStudent, який повністю наслідує клас Student
 
